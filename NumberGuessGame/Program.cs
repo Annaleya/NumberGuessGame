c7ca055f98cd370c5f1caa5af9b6f124bbb0c24a
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-06-03 16:39:39 -0400

    Adding score tracking

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -11,6 +11,8 @@ namespace NumberGuessGame
             int winNumber = number.Next(0, 10);
             int numberOfGuess = 0;
             int maxGuess = 3;
+            int winTotal = 0;
+            int roundTotal = 0;
             bool win = false;
 
 
@@ -24,26 +26,32 @@ namespace NumberGuessGame
 
                 if (numberOfGuess > maxGuess-2)
                 {
+                    roundTotal++;
                     Console.WriteLine("You have reached maximum number of guesses: " + maxGuess);
                     break;
                 }
 
                 else if (i > winNumber)
                     {
-                    numberOfGuess++;
+                        numberOfGuess++;
                         Console.WriteLine("To high, guess lower...");
                     }
                     else if (i < winNumber)
                     {
-                    numberOfGuess++;
+                        numberOfGuess++;
                         Console.WriteLine("To low, guess higher...");
                     }
                     else if (i == winNumber)
                     {
+                        roundTotal++;
+                        winTotal++;
                         Console.WriteLine("You win!");
                         win = true;
                     }
                 } while (win == false);
+
+            Console.WriteLine("Total wins: " + winTotal);
+            Console.WriteLine("Total plays: " + roundTotal);
 
                 Console.WriteLine("Press space to restart");
                 ConsoleKeyInfo cki = Console.ReadKey(true);
