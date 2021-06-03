5d8c1369f767579557b7c0ae099406d3ab4a62a5
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-06-03 15:46:35 -0400

    Limit number of guesses allowed

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -9,38 +9,49 @@ namespace NumberGuessGame
             Random number = new Random();
 
             int winNumber = number.Next(0, 10);
-
+            int numberOfGuess = 0;
+            int maxGuess = 3;
             bool win = false;
 
-            do
-            {
-                Console.WriteLine("Guess a number between 0 and 10: ");
-                string s = Console.ReadLine();
 
-                int i = int.Parse(s);
 
-                if (i > winNumber)
+                do
                 {
-                    Console.WriteLine("To high, guess lower...");
-                }
-                else if (i < winNumber)
-                {
-                    Console.WriteLine("To low, guess higher...");
-                }
-                else if (i == winNumber)
-                {
-                    Console.WriteLine("You win!");
-                    win = true;
-                }
-            } while (win == false);
+                    Console.WriteLine("Guess a number between 0 and 10: ");
+                    string s = Console.ReadLine();
 
-            Console.WriteLine("Press space to restart");
-            ConsoleKeyInfo cki = Console.ReadKey(true);
-            while (cki.Key != ConsoleKey.Escape)
-            {
-                if (cki.Key == ConsoleKey.Spacebar) Main();
-                cki = Console.ReadKey(true);
-            }
+                    int i = int.Parse(s);
+
+                if (numberOfGuess > maxGuess-2)
+                {
+                    Console.WriteLine("You have reached maximum number of guesses: " + maxGuess);
+                    break;
+                }
+
+                else if (i > winNumber)
+                    {
+                    numberOfGuess++;
+                        Console.WriteLine("To high, guess lower...");
+                    }
+                    else if (i < winNumber)
+                    {
+                    numberOfGuess++;
+                        Console.WriteLine("To low, guess higher...");
+                    }
+                    else if (i == winNumber)
+                    {
+                        Console.WriteLine("You win!");
+                        win = true;
+                    }
+                } while (win == false);
+
+                Console.WriteLine("Press space to restart");
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                while (cki.Key != ConsoleKey.Escape)
+                {
+                    if (cki.Key == ConsoleKey.Spacebar) Main();
+                    cki = Console.ReadKey(true);
+                }
         }
     }
 }
