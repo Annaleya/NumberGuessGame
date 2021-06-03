78674d2b30c42b8aa1c770e7490b438e727ef246
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-06-03 15:21:11 -0400

    Create number guess game

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -6,7 +6,33 @@ namespace NumberGuessGame
     {
         public static void Main(string[] args)
         {
-           
+            Random number = new Random();
+
+            int winNumber = number.Next(0, 10);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 10: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNumber)
+                {
+                    Console.WriteLine("To high, guess lower...");
+                }
+                else if (i < winNumber)
+                {
+                    Console.WriteLine("To low, guess higher...");
+                }
+                else if (i == winNumber)
+                {
+                    Console.WriteLine("You win!");
+                    win = true;
+                }
+            }
         }
     }
 }
