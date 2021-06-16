91c74c318695fd2453aa9187c4b2e0929ddce105
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-06-16 15:23:09 -0400

    Clean up code and setup for better build out

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -2,57 +2,119 @@
 
 namespace NumberGuessGame
 {
-    class MainClass
+    class Program
     {
+        int winNumber;
+        int guess;
+        int numberOfGuess = 0;
+        int maxGuess = 3;
+        int guessTotal = 0;
+        int correctGuess = 0;
+
         public static void Main()
         {
-            Random number = new Random();
+            Program program = new Program();
+            program.Welcome();
+            program.random();
 
-                int winNumber = number.Next(0, 10);
-                int numberOfGuess = 0;
-                int maxGuess = 3;
-                bool win = false;
+            while (true)
+            {
+                program.userData();
+                program.Game();
+            }
+        }
 
-
-                do
+        //method to start the game
+        private void Welcome()
+        {
+            Console.WriteLine("Welcome to the Random Number Game! To continue, Press enter...");
+            Console.ReadLine();
+        }
+        //prompt user for guess
+        private void userData()
+        {
+            Console.WriteLine("Guess a number between 0 and 10:");
+            guess = int.Parse(Console.ReadLine());
+        }
+        //method for displaying correct or incorrect answers
+        private void Game()
+        {
+            //limit number of total guesses
+            if (numberOfGuess > maxGuess -2)
+            { 
+                Console.WriteLine("Try Again! You have reached maximum number of guesses: " + maxGuess);
+                PlayAgain();
+            }
+            //method for correct guess
+            else if (guess == winNumber)
+            {
+                correctGuess++;
+                guessTotal++;
+                numberOfGuess++;
+                Console.WriteLine("You win!");
+                Console.WriteLine("Total Guesses:" + guessTotal);
+                Console.WriteLine("Correct Guesses: " + correctGuess);
+                PlayAgain();
+            }
+            //method for incorrect guess(to high)
+            else if (guess > winNumber)
+            {
+                guessTotal++;
+                numberOfGuess++;
+                Console.WriteLine("To high, guess lower...");
+            }
+            //method for incorrect guess(to low)
+            else if (guess < winNumber)
+            {
+                guessTotal++;
+                numberOfGuess++;
+                Console.WriteLine("To low, guess higher...");
+            }
+        }
+        //method for tracking score
+        private void Score()
+        {
+            Console.WriteLine("Total Guesses: " + guessTotal);
+            Console.WriteLine("Correct Guesses: " + correctGuess);
+        }
+        //method for restarting the game
+        private void PlayAgain()
+        {
+            Console.WriteLine("Would you like to restart? Yes or No");
+            string respond = Console.ReadLine().ToLower();
+            if (respond == "yes")
+            {
+                Welcome();
+                random();
+                while (true)
                 {
-                    Console.WriteLine("Guess a number between 0 and 10: ");
-                    string s = Console.ReadLine();
-
-                    int i = int.Parse(s);
-
-                    if (numberOfGuess > maxGuess - 2)
-                    {
-                        Console.WriteLine("You have reached maximum number of guesses: " + maxGuess);
-                        break;
-                    }
-
-                    else if (i > winNumber)
-                    {
-                        numberOfGuess++;
-                        Console.WriteLine("To high, guess lower...");
-                    }
-                    else if (i < winNumber)
-                    {
-                        numberOfGuess++;
-                        Console.WriteLine("To low, guess higher...");
-                    }
-                    else if (i == winNumber)
-                    {
-                        Console.WriteLine("You win!");
-                        win = true;
-                    }
-                } while (win == false);
-
-                Console.WriteLine("Press space to restart");
-                ConsoleKeyInfo cki = Console.ReadKey(true);
-                while (cki.Key != ConsoleKey.Escape)
-                {
-                    if (cki.Key == ConsoleKey.Spacebar) Main();
-                cki = Console.ReadKey(true);
+                    userData();
+                    Game();
                 }
-                }
-            
-        }   
-    }
+            }
+            else if (respond == "no")
+            {
+                EndProgram();
+            }
+            else
+            {
+                Console.WriteLine("Your response wasn't recognized...");
+                Console.WriteLine("Game is now closing");
+                EndProgram();
+            }
+        }
+        //method for closing program
+        private void EndProgram()
+        {
+            Console.WriteLine("/n");
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+        }
+        //method for random number generator
+        private void random()
+        {
+            Random random = new Random();
+            winNumber = random.Next(0, 10);
+        }
+    }   
 }
