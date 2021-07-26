7ef87e475305db912e5e53d22b6822ba4793861d
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-07-25 20:54:19 -0400

    Revert "clean up code"
    This reverts commit 94e6ec784a46692b6618412ad99195b453a77887.

--- NumberGuessGame/Program.cs
+++ NumberGuessGame/Program.cs
@@ -4,49 +4,24 @@ namespace NumberGuessGame
 {
     class Program
     {
+        int winNumber;
+        int guess;
+        int numberOfGuess = 0;
+        int maxGuess = 3;
+        int guessTotal = 0;
         int correctGuess = 0;
 
         public static void Main()
         {
             Program program = new Program();
-            program.Play();
-        }
-
-        //method for restarting the game
-        private void Play()
-        {
-            Welcome();
-            int levelMinNumber = 0;
-            int levelMaxNumber = 10;
-            int maxGuess = 3;
+            program.Welcome();
+            program.random();
 
             while (true)
             {
-                int winNumber = random(levelMinNumber, levelMaxNumber);
-
-                //one game played here
-
-                bool continuePlaying = true;
-                int guessTotal = 0;
-
-                while (continuePlaying)
-                {
-                    int guess = userData();
-                    guessTotal++;
-                    continuePlaying = CheckInput(guess, guessTotal, winNumber, maxGuess);
-                }
-
-                Console.WriteLine("Would you like to restart? Yes or No");
-                string respond = Console.ReadLine().ToLower();
-                if (respond == "yes")
-                {
-                    continue;
-                }
-                //user did not want to play again
-                break;
+                program.userData();
+                program.Game();
             }
-
-            EndProgram();
         }
 
         //method to start the game
@@ -56,60 +31,90 @@ namespace NumberGuessGame
             Console.ReadLine();
         }
         //prompt user for guess
-        private int userData()
+        private void userData()
         {
             Console.WriteLine("Guess a number between 0 and 10:");
-            return int.Parse(Console.ReadLine());
+            guess = int.Parse(Console.ReadLine());
         }
-
-        /// <summary>
-        ///  Checks the guess and tells user if they won, lost or guessed incorrectly
-        /// </summary
-        /// <param name="guess"></param>
-        /// <returns>True if game is continuing</returns>
-        private bool CheckInput(int guess, int totalGuesses, int winNumber, int maxGuess)
+        //method for displaying correct or incorrect answers
+        private void Game()
         {
-            bool continueGame = true;
+            //limit number of total guesses
+            if (numberOfGuess > maxGuess -2)
+            { 
+                Console.WriteLine("Try Again! You have reached maximum number of guesses: " + maxGuess);
+                Score();
+                PlayAgain();
+            }
             //method for correct guess
-            if (guess == winNumber)
+            else if (guess == winNumber)
             {
                 correctGuess++;
+                guessTotal++;
+                numberOfGuess++;
                 Console.WriteLine("You win!");
-                Console.WriteLine("Total Guesses:" + totalGuesses);
+                Console.WriteLine("Total Guesses:" + guessTotal);
                 Console.WriteLine("Correct Guesses: " + correctGuess);
-                continueGame = false;
+                PlayAgain();
             }
-
             //method for incorrect guess(to high)
-            if (guess > winNumber)
+            else if (guess > winNumber)
             {
+                guessTotal++;
+                numberOfGuess++;
                 Console.WriteLine("To high, guess lower...");
             }
             //method for incorrect guess(to low)
             else if (guess < winNumber)
             {
+                guessTotal++;
+                numberOfGuess++;
                 Console.WriteLine("To low, guess higher...");
             }
-            //limit number of total guesses
-            if (continueGame && totalGuesses >= maxGuess)
-            {
-                Console.WriteLine("Try Again! You have reached maximum number of guesses: " + maxGuess);
-                Score(totalGuesses, correctGuess);
-                continueGame = false;
-            }
-
-            return continueGame;
         }
 
         private void Level()
         {
+            if (guess == winNumber)
+            {
 
+            }
+            else if (guess !== winNumber)
+            {
+
+            }
         }
         //method for tracking score
-        private void Score(int totalGuesses, int correctGuesses)
+        private void Score()
         {
-            Console.WriteLine("Total Guesses: " + totalGuesses);
-            Console.WriteLine("Correct Guesses: " + correctGuesses);
+            Console.WriteLine("Total Guesses: " + guessTotal);
+            Console.WriteLine("Correct Guesses: " + correctGuess);
+        }
+        //method for restarting the game
+        private void PlayAgain()
+        {
+            Console.WriteLine("Would you like to restart? Yes or No");
+            string respond = Console.ReadLine().ToLower();
+            if (respond == "yes")
+            {
+                Welcome();
+                random();
+                while (true)
+                {
+                    userData();
+                    Game();
+                }
+            }
+            else if (respond == "no")
+            {
+                EndProgram();
+            }
+            else
+            {
+                Console.WriteLine("Your response wasn't recognized...");
+                Console.WriteLine("Game is now closing");
+                EndProgram();
+            }
         }
         //method for closing program
         private void EndProgram()
@@ -119,10 +124,10 @@ namespace NumberGuessGame
             Console.ReadKey();
         }
         //method for random number generator
-        private int random(int min, int max)
+        private void random()
         {
             Random random = new Random();
-            return random.Next(min, max);
+            winNumber = random.Next(0, 10);
         }
     }   
 }
